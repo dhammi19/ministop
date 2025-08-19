f7f7d0ef1cfca5460afda53582523839996062d7
Đào Hồ Anh <daohoanh.work@gmail.com>
2025-08-19 22:18:22 +0700

    First commit - Spring Boot project

---
+++ src/main/java/com/example/ministop/MinistopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ministop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ministop/MinistopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ministop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
