8c87a67355e45720037285188307b2c8b56c98aa
Đào Hồ Anh <daohoanh.work@gmail.com>
2025-08-20 23:08:16 +0700

    Update EmployeeRole

--- src/main/java/com/example/ministop/entity/EmployeeRole.java
+++ src/main/java/com/example/ministop/entity/EmployeeRole.java
@@ -1,8 +1,14 @@
 package com.example.ministop.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity(name = "EmployeeRole")
 public class EmployeeRole {
+    @Id
+    private String roleId;
 
+    @Column(name = "role_name")
+    private String roleName;
 }
